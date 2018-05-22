7a2896db73380322212ad44ca2d22ad8b5cb0aa8
celtare21 <celtare21@gmail.com>
2018-05-22 23:14:33 +0000

    AICP-ify
    Change-Id: I27b47b1cee2fb7089a653f30631e42958c9a26f8
    Signed-off-by: celtare21 <celtare21@gmail.com>

--- aicp_mata.mk
+++ aicp_mata.mk
@@ -1,6 +1,6 @@
 # Inherit some common Lineage stuff.
 $(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
-$(call inherit-product, vendor/lineage/config/common_full_phone.mk)
+$(call inherit-product, vendor/aicp/config/common_full_phone.mk)
 
 $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/languages_full.mk)
@@ -20,7 +20,7 @@ endif
 
 # Device identifiers
 PRODUCT_DEVICE := mata
-PRODUCT_NAME := lineage_mata
+PRODUCT_NAME := aicp_mata
 PRODUCT_BRAND := essential
 PRODUCT_MODEL := PH-1
 PRODUCT_MANUFACTURER := Essential Products
@@ -31,3 +31,7 @@ PRODUCT_BUILD_PROP_OVERRIDES += \
         PRIVATE_BUILD_DESC="mata-user 9 PPR1.181005.099 413 release-keys"
 
 BUILD_FINGERPRINT := essential/mata/mata:9/PPR1.181005.099/413:user/release-keys
+
+# AICP Device Maintainers
+PRODUCT_BUILD_PROP_OVERRIDES += \
+    DEVICE_MAINTAINERS="Vlad Adumitroaie (celtare21)"
