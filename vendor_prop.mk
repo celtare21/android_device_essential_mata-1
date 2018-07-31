ebb778aebdfbff6f06adfeb9b079ce93b6868d7d
celtare21 <celtare21@gmail.com>
2018-07-31 23:26:16 +0000

    vendor.prop: Enable EIS
    Change-Id: Icf5ee4e9218cd2597a787741d7fb62ae92b8f5b9
    Signed-off-by: celtare21 <celtare21@gmail.com>

--- vendor_prop.mk
+++ vendor_prop.mk
@@ -47,7 +47,9 @@ PRODUCT_PROPERTY_OVERRIDES += \
     vendor.camera.aux.packagelist="org.codeaurora.snapcam,com.essential.klik,org.lineageos.snap" \
     persist.camera.camera2=true \
     persist.camera.expose.aux=1 \
-    persist.camera.is_type=3 \
+    persist.camera.is_type=4 \
+    persist.camera.is_mode=4 \
+    persist.camera.eis.enable=1 \
     persist.camera.max.previewfps=60 \
     vidc.enc.dcvs.extra-buff-count=2
 
