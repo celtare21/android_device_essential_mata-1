ce1bec54ab17fbc050e1f2bb04aedc323e927b93
celtare21 <celtare21@gmail.com>
2018-08-16 19:31:42 +0000

    Boardconfig: Disable schedstats
    Signed-off-by: celtare21 <celtare21@gmail.com>

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -139,7 +139,7 @@ TARGET_FS_CONFIG_GEN := $(DEVICE_PATH)/config.fs
 TARGET_USES_INTERACTION_BOOST := true
 
 # Kernel
-BOARD_KERNEL_CMDLINE := androidboot.hardware=mata sched_enable_hmp=1 sched_enable_power_aware=1 ehci-hcd.park=3
+BOARD_KERNEL_CMDLINE := androidboot.hardware=mata sched_enable_hmp=1 sched_enable_power_aware=1 ehci-hcd.park=3 schedstats=disable
 BOARD_KERNEL_CMDLINE += service_locator.enable=1
 BOARD_KERNEL_CMDLINE += swiotlb=2048 androidboot.configfs=true
 BOARD_KERNEL_CMDLINE += androidboot.usbcontroller=a800000.dwc3 cma=32M@0-0xffffffff
