43a3d2cc6aee4e0bd9381d2396e85524c1b1bc51
celtare21 <celtare21@gmail.com>
2018-09-04 17:09:04 +0000

    Make Doze and QTI buildable
    Signed-off-by: celtare21 <celtare21@gmail.com>

--- doze/Android.mk
+++ doze/Android.mk
@@ -5,6 +5,7 @@ LOCAL_MODULE_TAGS := optional
 
 LOCAL_SRC_FILES := $(call all-java-files-under, src)
 
+LOCAL_PRIVATE_PLATFORM_APIS := true
 LOCAL_PACKAGE_NAME := Doze
 LOCAL_CERTIFICATE := platform
 LOCAL_PRIVILEGED_MODULE := true
