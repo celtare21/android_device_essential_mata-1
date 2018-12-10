18efe89773a30c362a125cd81f8860c729abc07d
Ethan Chen <intervigil@gmail.com>
2018-12-09 19:54:31 -0800

    mata: Remove libhidl hacks
    * The stock libraries don't appear to have this dependency anymore.
    
    Change-Id: Ibdc3d27c536dce7c2536dc617f712259687c2ae8
    Signed-off-by: celtare21 <celtare21@gmail.com>

--- device.mk
+++ device.mk
@@ -197,13 +197,6 @@ PRODUCT_COPY_FILES += \
 PRODUCT_PACKAGES += \
     android.hardware.health@2.0-service.mata
 
-# HIDL
-PRODUCT_PACKAGES += \
-    android.hidl.base@1.0 \
-    android.hidl.base@1.0_system \
-    android.hidl.manager@1.0 \
-    android.hidl.manager@1.0-java
-
 # IMS
 PRODUCT_PACKAGES += \
     ims-ext-common
