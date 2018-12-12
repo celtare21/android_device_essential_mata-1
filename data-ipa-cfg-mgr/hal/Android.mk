d322257123c3aaa8352fec6d6732854942b3e8cc
celtare21 <celtare21@gmail.com>
2018-12-12 09:52:48 +0000

    mata: data-ipa-cfg-mgr: Cleanup
    *Enable Werror
    *Disable Debugging
    *Fix memory leaks
    
    Signed-off-by: celtare21 <celtare21@gmail.com>

--- data-ipa-cfg-mgr/hal/Android.mk
+++ data-ipa-cfg-mgr/hal/Android.mk
@@ -11,7 +11,7 @@ LOCAL_SRC_FILES := src/CtUpdateAmbassador.cpp \
 LOCAL_C_INCLUDES := $(LOCAL_PATH)/inc
 LOCAL_MODULE := liboffloadhal
 
-#LOCAL_CPP_FLAGS := -Wall -Werror
+LOCAL_CPP_FLAGS := -Wall -Werror
 LOCAL_SHARED_LIBRARIES := libhwbinder \
                         libhidlbase \
                         libhidltransport \
