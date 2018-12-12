2826028b144fafb4799b0518b6e7bd93c023e4ca
celtare21 <celtare21@gmail.com>
2018-12-12 09:52:48 +0000

    mata: data-ipa-cfg-mgr: Cleanup
    *Enable Werror
    *Disable Debugging
    *Fix memory leaks
    
    Signed-off-by: celtare21 <celtare21@gmail.com>

--- data-ipa-cfg-mgr/ipacm/src/Android.mk
+++ data-ipa-cfg-mgr/ipacm/src/Android.mk
@@ -25,7 +25,7 @@ LOCAL_CFLAGS := -v
 LOCAL_CFLAGS += -DFEATURE_IPA_ANDROID
 LOCAL_CFLAGS += -DFEATURE_IPACM_RESTART
 LOCAL_CFLAGS += -DFEATURE_IPACM_HAL -Wall -Werror -Wno-error=macro-redefined
-ifneq (,$(filter userdebug eng, $(TARGET_BUILD_VARIANT)))
+ifneq (,$(filter eng, $(TARGET_BUILD_VARIANT)))
 LOCAL_CFLAGS += -DDEBUG
 endif
 
