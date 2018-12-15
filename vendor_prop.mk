97d353b293e7da2fafaee03a4834d92058012d61
celtare21 <celtare21@gmail.com>
2018-12-15 17:33:31 +0000

    mata: vendor_prop: Whitelist proper Gcam
    Signed-off-by: celtare21 <celtare21@gmail.com>

--- vendor_prop.mk
+++ vendor_prop.mk
@@ -44,7 +44,7 @@ PRODUCT_PROPERTY_OVERRIDES += \
 
 # Camera
 PRODUCT_PROPERTY_OVERRIDES += \
-    vendor.camera.aux.packagelist="org.codeaurora.snapcam,com.essential.klik,org.lineageos.snap" \
+    vendor.camera.aux.packagelist="org.codeaurora.snapcam,com.essential.klik,org.lineageos.snap,com.google.android.GoogleCamera" \
     persist.camera.camera2=true \
     persist.camera.expose.aux=1 \
     persist.camera.is_type=4 \
