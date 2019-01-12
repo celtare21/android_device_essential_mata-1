74e17fc2e5f73e149644d4471fb6ee5918c4de39
celtare21 <celtare21@gmail.com>
2019-01-12 11:36:20 +0000

    mata: use 8 cores instead of 4 for dex2oat
    Signed-off-by: celtare21 <celtare21@gmail.com>

--- vendor_prop.mk
+++ vendor_prop.mk
@@ -186,8 +186,7 @@ PRODUCT_PROPERTY_OVERRIDES += \
     ro.vendor.qti.am.reschedule_service=true \
     ro.sys.fw.dex2oat_thread_count=8 \
     dalvik.vm.boot-dex2oat-threads=8 \
-    dalvik.vm.dex2oat-threads=4 \
-    dalvik.vm.image-dex2oat-threads=4
+    dalvik.vm.dex2oat-threads=8
 
 # Vol steps
 PRODUCT_PROPERTY_OVERRIDES += \
