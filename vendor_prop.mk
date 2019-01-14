cea1c740b70d4193a908945992dc66a109db5c6e
celtare21 <celtare21@gmail.com>
2019-01-14 15:35:12 +0000

    mata: vendor_prop: Squashed tweaks
    Signed-off-by: celtare21 <celtare21@gmail.com>

--- vendor_prop.mk
+++ vendor_prop.mk
@@ -145,6 +145,7 @@ PRODUCT_PROPERTY_OVERRIDES += \
     persist.radio.videopause.mode=1 \
     persist.radio.sap_silent_pin=1 \
     persist.radio.always_send_plmn=true \
+    persist.radio.add_power_save=1 \
     persist.dbg.ims_volte_enable=1
 
 
@@ -178,6 +179,26 @@ PRODUCT_PROPERTY_OVERRIDES += \
 PRODUCT_PROPERTY_OVERRIDES += \
     ro.min.fling_velocity=160 \
     ro.max.fling_velocity=20000
+
+# GPU
+PRODUCT_PROPERTY_OVERRIDES += \
+    debug.composition.type=c2d \
+    debug.mdpcomp.idletime=600 \
+    persist.hwc.mdpcomp.enable=true \
+    persist.hwc.ptor.enable=true \
+    debug.enable.sglscale=1 \
+    debug.sf.hw=1 \
+    debug.hwui.renderer=opengl \
+    debug.egl.hw=1 \
+    debug.sf.disable_hwc=0 \
+    debug.sf.recomputecrop=0 \
+    debug.sf.disable_backpressure=1 \
+    debug.sf.latch_unsignaled=1 \
+    persist.hwc.mdpcomp.enable=true \
+    persist.hwc.ptor.enable=true \
+    debug.sf.gpu_comp_tiling=1 \
+    debug.performance.tuning=1 \
+    video.accelerate.hw=1
 
 # ART
 PRODUCT_PROPERTY_OVERRIDES += \
