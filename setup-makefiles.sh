acc39afaa6bf74633db1c318bba44284e1d0f04d
itz63c <44932193+itz63c@users.noreply.github.com>
2019-01-18 23:51:04 -0500

    aosip-ify

--- setup-makefiles.sh
+++ setup-makefiles.sh
@@ -27,9 +27,9 @@ INITIAL_COPYRIGHT_YEAR=2017
 MY_DIR="${BASH_SOURCE%/*}"
 if [[ ! -d "$MY_DIR" ]]; then MY_DIR="$PWD"; fi
 
-AICP_ROOT="$MY_DIR"/../../..
+AOSIP_ROOT="$MY_DIR"/../../..
 
-HELPER="$AICP_ROOT"/vendor/aicp/build/tools/extract_utils.sh
+HELPER="$AOSIP_ROOT"/vendor/aosip/build/tools/extract_utils.sh
 if [ ! -f "$HELPER" ]; then
     echo "Unable to find helper script at $HELPER"
     exit 1
@@ -37,7 +37,7 @@ fi
 . "$HELPER"
 
 # Initialize the helper
-setup_vendor "$DEVICE" "$VENDOR" "$AICP_ROOT"
+setup_vendor "$DEVICE" "$VENDOR" "$AOSIP_ROOT"
 
 # Copyright headers and guards
 write_headers
