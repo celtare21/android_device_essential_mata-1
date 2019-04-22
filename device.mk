1149320d642b195091a739766a040bcc6631871e
Jack Yu <jackcwyu@google.com>
2019-04-22 20:10:48 +0800

    mata: Update nfc@1.1 hal to nfc@1.2 and disable P2P polling mask
    Bug: 130509605
    Test: Nfc on/off, CE/Tag
    Change-Id: I2464508966413fee54f8886518e63e9d54533025
    Signed-off-by: celtare21 <celtare21@gmail.com>

--- device.mk
+++ device.mk
@@ -233,7 +233,7 @@ PRODUCT_PACKAGES += \
 
 # NFC
 PRODUCT_PACKAGES += \
-    android.hardware.nfc@1.1-service \
+    android.hardware.nfc@1.2-service \
     com.android.nfc_extras \
     NfcNci \
     SecureElement \
