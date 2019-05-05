fe0731126725def9edc462b1c5ddb2d301e81f0a
celtare21 <celtare21@gmail.com>
2019-05-05 09:59:37 +0000

    Revert "mata: enable cpuidle during boot"
    This reverts commit df31f394d058903993f072e7a9bb630da26051ef.
    
    Signed-off-by: celtare21 <celtare21@gmail.com>

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -162,7 +162,7 @@ TARGET_FS_CONFIG_GEN := $(DEVICE_PATH)/config.fs
 
 # Kernel
 BOARD_KERNEL_CMDLINE := androidboot.hardware=mata ehci-hcd.park=3 schedstats=disable
-BOARD_KERNEL_CMDLINE += service_locator.enable=1
+BOARD_KERNEL_CMDLINE += lpm_levels.sleep_disabled=1 service_locator.enable=1
 BOARD_KERNEL_CMDLINE += loop.max_part=7
 BOARD_KERNEL_CMDLINE += swiotlb=2048 androidboot.configfs=true
 BOARD_KERNEL_CMDLINE += androidboot.usbcontroller=a800000.dwc3 cma=32M@0-0xffffffff
