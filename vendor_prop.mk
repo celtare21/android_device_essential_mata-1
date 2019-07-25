62176a6e6e98975919a98b7db394143cc22ed42c
celtare21 <celtare21@gmail.com>
2019-07-25 21:11:01 +0000

    mata: Update props
    SERVICE_RESCHEDULE is for devices with 2GB or less memory.
    
    Signed-off-by: celtare21 <celtare21@gmail.com>

--- vendor_prop.mk
+++ vendor_prop.mk
@@ -165,7 +165,6 @@ PRODUCT_PROPERTY_OVERRIDES += \
 PRODUCT_PROPERTY_OVERRIDES += \
     dalvik.vm.dex2oat-filter=speed \
     dalvik.vm.image-dex2oat-filter=speed \
-    ro.vendor.qti.am.reschedule_service=true \
     ro.sys.fw.dex2oat_thread_count=8 \
     dalvik.vm.boot-dex2oat-threads=8 \
     dalvik.vm.dex2oat-threads=8 \
