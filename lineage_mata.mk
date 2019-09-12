90021bad8cbf21ca02c4561e4dfe09eeff029803
Bowgo Tsai <bowgotsai@google.com>
2019-09-12 13:28:00 +0800

    Revert "mata: Adding GSI keys"
    This reverts commit 421986004d9044f8deed5aa4e90be27dda4d01c8.
    Wahoo has no first-stage ramdisk and there is no need to install
    GSI keys there.
    
    Bug: 140873290
    Test: tree hugger
    
    Change-Id: I4dfe0bb0d88dbbbcaa6742cef9f2405d3b7936d6
    Signed-off-by: celtare21 <celtare21@gmail.com>

--- lineage_mata.mk
+++ lineage_mata.mk
@@ -9,9 +9,6 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/product_launched_with_n_mr1.mk
 # Vendor blobs
 $(call inherit-product-if-exists, vendor/essential/mata/mata-vendor.mk)
 
-# Installs gsi keys into ramdisk, to boot a GSI with verified boot.
-$(call inherit-product, $(SRC_TARGET_DIR)/product/gsi_keys.mk)
-
 # Enable updating of APEXes
 $(call inherit-product, $(SRC_TARGET_DIR)/product/updatable_apex.mk)
 
