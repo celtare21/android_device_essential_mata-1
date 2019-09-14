e22697eb5fdfdf290fd6b1777ee8d860b2b33555
celtare21 <celtare21@gmail.com>
2019-09-14 10:13:11 +0000

    [DND]mata: Keep libnfc-nci.conf in product and add manifest entry for nxpese
    Signed-off-by: celtare21 <celtare21@gmail.com>

--- device.mk
+++ device.mk
@@ -240,7 +240,7 @@ PRODUCT_PACKAGES += \
     Tag
 
 PRODUCT_COPY_FILES += \
-    device/essential/mata/configs/libnfc-nci.conf:$(TARGET_COPY_OUT_SYSTEM)/etc/libnfc-nci.conf \
+    device/essential/mata/configs/libnfc-nci.conf:$(TARGET_COPY_OUT_PRODUCT)/etc/libnfc-nci.conf \
     device/essential/mata/configs/libnfc-nxp.conf:$(TARGET_COPY_OUT_VENDOR)/etc/libnfc-nxp.conf
 
 # Overlay
