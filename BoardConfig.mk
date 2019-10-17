e859f431f7d80bb4de125fdbc45b3b625bc73ff5
Ethan Chen <intervigil@gmail.com>
2019-10-16 22:14:38 -0700

    mata: Remove CMA parameter
    * This parameter is no longer present in the commandline on the
      Essential stock firmware.
    
    Change-Id: I2bb63049a0a29d064f352cb7810ce513dfbb81c3
    Signed-off-by: celtare21 <celtare21@gmail.com>

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -159,7 +159,7 @@ BOARD_KERNEL_CMDLINE += lpm_levels.sleep_disabled=1 service_locator.enable=1
 BOARD_KERNEL_CMDLINE += service_locator.enable=1
 BOARD_KERNEL_CMDLINE += loop.max_part=7
 BOARD_KERNEL_CMDLINE += swiotlb=2048 androidboot.configfs=true
-BOARD_KERNEL_CMDLINE += androidboot.usbcontroller=a800000.dwc3 cma=32M@0-0xffffffff
+BOARD_KERNEL_CMDLINE += androidboot.usbcontroller=a800000.dwc3
 BOARD_KERNEL_CMDLINE += androidboot.selinux=permissive
 BOARD_KERNEL_BASE := 0x00000000
 BOARD_KERNEL_PAGESIZE := 4096
